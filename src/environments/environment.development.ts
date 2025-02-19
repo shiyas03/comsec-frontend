805175ee8ca4f4e74a50f178568e5341e29083c4
A4AMEEN <alameenmail07@gmail.com>
2025-02-19 22:40:22 +0530

    initial commit

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,6 +1,4 @@
 export const environment = {
     production: false,
-   // baseURL: "http://3.110.118.196/api/" 
-    // baseURL: "http://localhost:3000/"
      baseURL: "http://13.201.223.43:3000/"
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,5 @@
 export const environment = {
     production: true,
     baseURL: "http://13.201.223.43:3000/api/"
-    //  baseURL: "http://localhost:3000/"
     
 };
