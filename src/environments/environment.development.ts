34c9f941b67a5daf8a07d39eb25e0f987385305e
LIYANMUBARAK <115862601+LIYANMUBARAK@users.noreply.github.com>
2025-06-24 08:27:04 +0530

    intial commit

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
-   baseURL: "http://13.201.191.186:3000/"
-   //baseURL: "http://localhost:3000/"
+  //  baseURL: "http://13.201.191.186:3000/"
+   baseURL: "http://localhost:3000/"
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: true,
-      baseURL: "http://13.201.191.186:3000/"
-     //baseURL: "http://localhost:3000/"
+      // baseURL: "http://13.201.191.186:3000/"
+     baseURL: "http://localhost:3000/"
     
 };
